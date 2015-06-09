87a9f9e7e6aa1d5d10be61961825cceb1de22353
domantasjurkus <domantas.jurkus@gmail.com>
2015-06-09 07:28:08 +0100

    Port from env variable

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/views', 'index.html'));
-
-  //res.send('The / directory! Sent index.html here');
 });
 
-var server = app.listen(3000, function () {
-  console.log('Listening on port 3000');
-});
+var server = app.listen(process.env.PORT || 3000)
